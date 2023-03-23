664146df7518495f8716c644a7b7eb1769b20255
Analogium <lambertheo@gmail.com>
2023-03-23 17:15:48 +0100

    test

---
+++ client/angularcliproject/src/app/formticket/formticket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formticket',
+  templateUrl: './formticket.component.html',
+  styleUrls: ['./formticket.component.scss']
+})
+export class FormticketComponent {
+
+}
